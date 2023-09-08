a11d0c297343136544ffcfd6bca63eceb00bb66c
Jeff Nelson <jpnelson85@gmail.com>
2023-09-08 10:38:12 -0500

    add facade test

--- spec/facades/airbender_facade.rb
+++ spec/facades/airbender_facade.rb
@@ -1,8 +1,10 @@
-class AirbenderFacade
-  def characters_by_fire_nation
-    characters = AirbenderService.new.characters_by_nation("Fire Nation")
-    characters.map do |character_data|
-      Character.new(character_data)
+require "rails_helper"
+
+RSpec.describe AirbenderFacade do
+  describe "search" do
+    it "returns all characters of Fire Nation", :vcr do
+      characters = AirbenderFacade.new.characters_by_fire_nation
+      expect(characters).to be_an(Array)
     end
   end
 end
